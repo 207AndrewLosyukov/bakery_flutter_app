f06f124a8648bb5da1232ca412e4a7c6e4f470f6
Loskir <kl@sinker.ru>
2021-12-10 16:44:24 +0300

    Add store to dependencies

--- lib/dependencies.dart
+++ lib/dependencies.dart
@@ -1,7 +1,11 @@
+import 'package:redux/redux.dart';
 import 'package:shop_flutter_app/products_api.dart';
+import 'package:shop_flutter_app/redux/state.dart';
+import 'package:shop_flutter_app/redux/store.dart';
 
 class Dependencies {
   ProductsApi productsApi;
+  late final Store<GlobalState> store = MyStoreBuilder.build();
   static late Dependencies _instance;
 
   Dependencies._({required this.productsApi});
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:shop_flutter_app/dependencies.dart';
 import 'package:shop_flutter_app/screens/main.dart';
 
 void main() async {
   await Dependencies.init();
-
-  Dependencies.instance.productsApi;
   runApp(const MyApp());
 }
 
@@ -15,21 +14,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return StoreProvider(
+      store: Dependencies.instance.store,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const MainBottomNavigationBar(),
       ),
-      home: const MainBottomNavigationBar(),
     );
   }
 }
