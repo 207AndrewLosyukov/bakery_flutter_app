5bb7a631bd4b139b0571048a37b41cae295cc8d8
207AndrewLosyukov <avlosyukov@edu.hse.ru>
2021-12-16 23:08:36 +0300

    AuthScreen верстка

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -73,39 +73,6 @@ class _AuthScreenState extends State<AuthScreen> {
         return null;
       };
 
-  auth() async {
-    // try {
-    //   UserCredential userCredential =
-    //       await FirebaseAuth.instance.createUserWithEmailAndPassword(
-    //     email: "barry.allen@example.com",
-    //     password: "SuperSecretPassword!",
-    //   );
-    // } on FirebaseAuthException catch (e) {
-    //   if (e.code == 'weak-password') {
-    //     print('The password provided is too weak.');
-    //   } else if (e.code == 'email-already-in-use') {
-    //     print('The account already exists for that email.');
-    //   }
-    // } catch (e) {
-    //   print(e);
-    // }
-  }
-
-  signIn() async {
-    // try {
-    //   UserCredential userCredential = await FirebaseAuth.instance
-    //       .signInWithEmailAndPassword(
-    //           email: "barry.allen@example.com",
-    //           password: "SuperSecretPassword!");
-    // } on FirebaseAuthException catch (e) {
-    //   if (e.code == 'user-not-found') {
-    //     print('No user found for that email.');
-    //   } else if (e.code == 'wrong-password') {
-    //     print('Wrong password provided for that user.');
-    //   }
-    // }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -168,7 +135,7 @@ class _AuthScreenState extends State<AuthScreen> {
                       // the form is invalid.
                       if (_formKey.currentState!.validate()) {
                         // Сюда писать аутентификацию
-                        auth();
+                        // auth();
                       }
                     },
                     child: const Text('Submit'),
